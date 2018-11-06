c23f094efc372d2d211879c433ea7104b01918b7
Kyle Lin <kylejlin.us@gmail.com>
2018-11-05 19:37:46 -0800

    Refactor translate.js

--- src/nodeUtils/translate.js
+++ src/nodeUtils/translate.js
@@ -3,41 +3,24 @@ const browserProm = require("./browserProm");
 module.exports = async function(chinese) {
   const browser = await browserProm;
   const page = await browser.newPage();
-  await page.goto("https://translate.google.com");
-  await page.waitFor(() => document.querySelector("#gt-sl-gms"));
-  await page.click("#gt-sl-gms");
-  const id = await page.evaluate(() => {
-    return Array.from(
-      document.querySelectorAll(".goog-menuitem.goog-option"),
-    ).find(m => m.innerText === "Chinese").id;
-  });
-  await page.click('div[id="' + id + '"]');
-  await page.waitFor(() => document.querySelector("textarea"));
-  page.evaluate(chinese => {
-    const textarea = document.querySelector("textarea");
-    textarea.value = chinese;
-  }, chinese);
+  await page.goto(`https://translate.google.com/#zh-CN/en/${chinese}`);
   await page.waitFor(
     () =>
       document.querySelector("#result_box") &&
       document.querySelector("#result_box").innerText.length > 0,
   );
-  const meaning = await page.evaluate(() => {
-    const resultBox = document.querySelector("#result_box");
-    return resultBox.innerText;
-  });
+  const meaning = await page.evaluate(
+    () => document.querySelector("#result_box").innerText,
+  );
   await page.waitFor(
     () =>
       document.querySelector("#src-translit") &&
       document.querySelector("#src-translit").innerText.length > 0,
   );
-  const characters = await page.evaluate(() => {
-    const translitBox = document.querySelector("#src-translit");
-    return translitBox.innerText;
-  });
-  page.evaluate(characters => {
-    document.querySelector("textarea").value = characters;
-  }, characters);
+  const characters = await page.evaluate(
+    () => document.querySelector("#src-translit").innerText,
+  );
+  await page.goto(`https://translate.google.com/#zh-CN/en/${characters}`);
   await page.waitFor(
     characters =>
       document.querySelector("#src-translit") &&
